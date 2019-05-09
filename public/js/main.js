ab621ed3f7342e5c40d64088517d25f804646c08
brioche1703 <kevin.meniel@gmail.com>
2019-05-09 13:38:05 +0900

    refactoring code

--- public/js/main.js
+++ public/js/main.js
@@ -1,10 +1,7 @@
-import Compositor from './Compositor.js';
 import Entity from './Entity.js';
 import Timer from './Timer.js'
 import {loadLevel} from './loaders.js';
 import {createMario} from './entities.js';
-import {loadBackgroundSprites} from './sprites.js';
-import {createBackgroundLayer, createSpriteLayer} from './layers.js';
 
 import Keyboard from './KeyboardState.js';
 
@@ -15,19 +12,16 @@ const context = canvas.getContext('2d');
 
 Promise.all([
 	createMario(),
-	loadBackgroundSprites(),
 	loadLevel('1-1'),
 ])
-.then (([mario, sprites, level]) => {
-	const comp = new Compositor();
-
-	const backgroundLayer = createBackgroundLayer(level.backgrounds, sprites);
-	comp.layers.push(backgroundLayer);
+.then (([mario, level]) => {
 
 	const gravity = 2000;
-	mario.pos.set(64, 180);
+	mario.pos.set(64, 64);
+
+	level.entities.add(mario);
 		
-	const SPACE = 32
+	const SPACE = 32;
 	const input = new Keyboard();
 	input.addMapping(SPACE, keyState => {
 		if (keyState) {
@@ -39,13 +33,10 @@ Promise.all([
 	});
 	input.listenTo(window);
 
-	const spriteLayer = createSpriteLayer(mario);
-	comp.layers.push(spriteLayer);
-
 	const timer = new Timer(1/60);
 	timer.update = function update(deltaTime) {
-		mario.update(deltaTime);
-		comp.draw(context);
+		level.update(deltaTime);
+		level.comp.draw(context);
 		mario.vel.y += gravity * deltaTime;
 	}
 	
